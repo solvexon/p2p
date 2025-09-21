c477fb0094e21abdcda4ec4fb2087c10650cf1e7
P2PSmartchain <197464731+solvexon@users.noreply.github.com>
2025-09-21 20:33:00 +0530

    Fix logo path for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/p2p/client/',
+  base: '/p2p/client/', // Add '/client/' to the path
 })
